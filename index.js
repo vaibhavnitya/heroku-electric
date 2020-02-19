0332fdd86b844c114918d9a34a7577cc4fa22b1e
vaibhav <vaibhavnitya@gmail.com>
2020-02-19 16:48:57 +0530

    update 1

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const httpServer = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+httpServer.use(bodyParser.json());                                                   
+httpServer.use(bodyParser.urlencoded({     // to support URL-encoded bodies            
+	extended: true
+}));    
+httpServer.use(express.static(__dirname));
+
+httpServer.set('views', __dirname +'views');
+httpServer.set('view engine', 'ejs');
+httpServer.engine('html', require('ejs').renderFile);
+httpServer.use(express.static(path.join(__dirname, 'public')))
+
+httpServer.listen(PORT, function() {
+    log('HTTP server listening at port', PORT);
+});
+
+httpServer.get('/', function(req,res){
+  res.render('pages/index')
+});
+
+httpServer.post('/registerUser', function(req,res) {
+	var data = {
+		'username': req.body.username,
+		'fname': req.body.fname,
+		'lname': req.body.lname,
+		'password': req.body.password
+	},
+	err = {
+		'err': 'incomplete data'
+	};
+	if (req.body.username && req.body.fname && req.body.lname && req.body.password) {
+    res.send({'err': null, 'message': 'successfully registered user'});
+	} else {
+		res.send(err);
+	}
+});

f2d2e8b8ddd5ca800049407d85beeb7ee053e295
vaibhav naik <vaibhavnitya@gmail.com>
2020-04-11 20:51:46 +0530

    update blockchain dependency

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const bodyParser = require('body-parser')
 const httpServer = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const server = require('./server')
 let BlockchainModule = null
 try {
   BlockchainModule = require('../sample-blockchain')
 } catch(err) {
   console.log('Missing dependency! This application is dependent on another repository.')
   console.log('To proceed further, clone "https://github.com/vaibhavnitya/sample-blockchain" repository in the parent directory.')
+  process.exit(1)
 }
+const server = require('./server')
 
 httpServer.use(bodyParser.json())                                 
 httpServer.use(bodyParser.urlencoded({     // to support URL-encoded bodies            
